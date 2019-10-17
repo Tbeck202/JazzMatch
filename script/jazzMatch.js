e82d9fb303f4f5626fc539b25596e3d284fcb526
Tyler Beck <tbeck202@gmail.com>
2019-10-16 17:22:29 -0700

    update JS, link images

---
+++ script/jazzMatch.js
@@ -0,0 +1,27 @@
+var players = [
+    {
+        name: "Adam Keefe",
+        number: 31,
+        difficulty: "as",
+    },
+    {
+        name: "Alec Burks",
+        number: 10,
+        difficulty: "ca",
+    },
+    {
+        name: "Andrei Kirilenko",
+        number: 47,
+        difficulty: "ca",
+    },
+    {
+        name: "Karl Malone",
+        number: 32,
+        difficulty: "bw",
+    },
+    {
+        name: "John Stockton",
+        number: 12,
+        difficulty: "bw",
+    }
+]
